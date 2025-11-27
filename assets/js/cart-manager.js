d7544e11dea2a81f81d62b72b95ad98a83e7d025
FrancoisCharlesATANGA <afrancoischarles@gmail.com>
2025-11-27 18:11:03 +0100

    ssdss

--- assets/js/cart-manager.js
+++ assets/js/cart-manager.js
@@ -28,13 +28,9 @@ class CartManager {
     // Ajouter un produit au panier
     // Ajouter un produit au panier
     addToCart(product) {
-<<<<<<< HEAD
-        const existingProduct = this.cart.find(item => item.id === product.id);
-
-=======
         const existingProduct = this.cart.find(item => item.name === product.name);
         
->>>>>>> 0ac1fe3d0ef0c2cb47586fa78a356b908516bab8
+
         if (existingProduct) {
             existingProduct.quantity += product.quantity || 1;
         } else {
